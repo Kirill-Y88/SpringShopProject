3a9daf0fef457fff1e8f5153a4acb7fb329575de
Kirill <benberov09@mail.ru>
2021-04-07 19:32:03 +0300

    Initial commit #1

---
+++ src/main/java/ru/geekbrains/springshop/SpringshopApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringshopApplication.class, args);
+    }
+
+}
